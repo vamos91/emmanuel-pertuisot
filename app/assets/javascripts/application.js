5477b6638af2702067914f177015fed45847b5b8
nicolas <acardnicolas@hotmail.com>
2018-05-08 13:05:40 +0200

    second commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,17 @@
 //= require rails-ujs
 //= require_tree .
+
+
+
+
+
+
+
+window.sr = ScrollReveal()
+sr.reveal('.nom', {origin:'bottom',delay: 500, distance: '10px', duration: 500, reset: false})
+sr.reveal('.fonction', {origin:'left',delay: 700, distance: '10px', duration: 1000, reset: false})
+sr.reveal('.nom2', {origin:'left',delay: 1000, distance: '100px', duration: 1000, reset: false})
+sr.reveal('.presentation', {origin:'right',delay: 1000, distance: '1000px', duration: 1000, reset: false})
+sr.reveal('.call-to-action', {origin:'bottom',delay: 1000, distance: '1000px', duration: 1000, reset: false})
+
+
