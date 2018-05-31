704165387bbd07093dd05f04c4a48878998e042d
nicolas <acardnicolas@hotmail.com>
2018-05-31 21:57:35 +0200

    gallery update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,7 @@ sr.reveal('.nom2', {origin:'left',delay: 1000, distance: '100px', duration: 1000
 sr.reveal('.presentation', {origin:'right',delay: 1000, distance: '1000px', duration: 1000, reset: false})
 sr.reveal('.call-to-action', {origin:'bottom',delay: 1000, distance: '1000px', duration: 1000, reset: false})
 sr.reveal('.punch-line2', {origin:'right',delay: 1000, distance: '1000px', duration: 1000, reset: false})
+
+
+
+
